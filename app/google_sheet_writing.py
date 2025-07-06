72e2b09260c082bf7eb48798e4f2b2a4366e013d
Jorge Plaza de los Reyes <jplaza@buk.cl>
2025-07-07 01:21:36 +0200

    build: encode google auth credentials

--- app/google_sheet_writing.py
+++ app/google_sheet_writing.py
@@ -1,11 +1,17 @@
 import gspread
+import os
+import json
+import base64
 from oauth2client.service_account import ServiceAccountCredentials
 
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1383yCHi58GlXC8xaAIHZQ4Pfian9LCptKaFgW3pO6Zk/edit?usp=sharing"
 
 def write_to_gsheet(data):
 	scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-	creds = ServiceAccountCredentials.from_json_keyfile_name("google_creds.json", scope)
+	b64_creds = os.environ.get("GOOGLE_CREDS_BASE64")
+	creds_json = json.loads(base64.b64decode(b64_creds))
+	
+	creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
 	client = gspread.authorize(creds)
 
 	sheet = client.open_by_url(SHEET_URL)
